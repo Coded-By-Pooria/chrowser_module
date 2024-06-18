e0b84b89b9c4e3b4e215754fc050b1b1de6c59b7
Pooria <166955989+Coded-By-Pooria@users.noreply.github.com>
2024-06-18 14:04:34 +0330

    V 1.2.0
    + Accessing nodes of web pages which could pass them to evaluate on them
    + Bug fixes

--- chrowser.d.ts
+++ chrowser.d.ts
@@ -1,14 +1,60 @@
-import CDP from 'chrome-remote-interface';
 import Protocol from 'devtools-protocol';
+import CDP from 'chrome-remote-interface';
+import ProtocolProxyApi from 'devtools-protocol/types/protocol-proxy-api';
+
+declare class ExecutionContext {
+    private context;
+    private id;
+    constructor(context: CDP.Client, id: number);
+    get executionContextId(): number;
+    private runExpression;
+    private runFunction;
+    private chechContext;
+    evaluate<T extends TabEvaluateFunction | string>(returnRemoteObject: boolean, script: T, ...args: any[]): Promise<EvaledType<T>>;
+    releaseRO(ro: RemoteNodeDelegator): Promise<void>;
+}
+type EvaledType<T extends TabEvaluateFunction | string> = T extends string ? any : Awaited<ReturnType<Exclude<T, string>>> extends infer X ? X extends Node ? RemoteNodeDelegator<X> : X extends (Node | null)[] ? (RemoteNodeDelegator<NonNullable<X[number]>> | null)[] : never : never;
+
+declare class RemoteObjectDelegator {
+    private ro;
+    constructor(ro: Protocol.Runtime.RemoteObject);
+    get id(): string;
+    get type(): "string" | "number" | "bigint" | "boolean" | "symbol" | "undefined" | "object" | "function";
+    get subType(): "array" | "null" | "node" | "regexp" | "date" | "map" | "set" | "weakmap" | "weakset" | "iterator" | "generator" | "error" | "proxy" | "promise" | "typedarray" | "arraybuffer" | "dataview" | "webassemblymemory" | "wasmvalue" | undefined;
+    get description(): string | undefined;
+}
+
+declare class RemoteNodeDelegator<T extends Node = HTMLElement> extends RemoteObjectDelegator implements Evaluable {
+    #private;
+    private context;
+    constructor(context: ExecutionContext, ro: Protocol.Runtime.RemoteObject);
+    evaluate(script: string | TabEvaluateFunction, ...args: any[]): Promise<any>;
+    $(selector: string): Promise<RemoteNodeDelegator | null>;
+    click(): Promise<void>;
+    $$(selector: string): Promise<RemoteNodeDelegator[]>;
+    $evaluate<T extends TabEvaluateFunction>(selector: string, handler: T): Promise<ReturnType<T>>;
+    $$evaluate<T extends TabEvaluateFunction>(selector: string, handler: T): Promise<ReturnType<T>[]>;
+    private released;
+    release(): Promise<void>;
+}
+
+interface Evaluable {
+    evaluate<T extends TabEvaluateFunction>(script: T | string, ...args: any[]): Promise<Awaited<ReturnType<T>>>;
+    $(selector: string): Promise<RemoteNodeDelegator | null>;
+    $$(selector: string): Promise<RemoteNodeDelegator[]>;
+    $evaluate<T extends TabEvaluateFunction<RemoteNodeDelegator<HTMLElement>>>(selector: string, handler: T): Promise<ReturnType<T>>;
+    $$evaluate<T extends TabEvaluateFunction<RemoteNodeDelegator<HTMLElement>[]>>(selector: string, handler: T): Promise<ReturnType<T>[]>;
+}
+
+type Input = CDP.DoEventPromises<'Input'> & CDP.DoEventListeners<'Input'> & CDP.AddOptParams<CDP.OptIfParamNullable<ProtocolProxyApi.InputApi>>;
 
 interface TabMouseBaseOptions {
     x: number;
     y: number;
 }
 declare class TabMouseHandler {
-    private tabId;
-    private tabHelper;
-    constructor(tabId: string, tabHelper: TabSessionZoneMaker);
+    private inputContext;
+    constructor(inputContext: Input);
     move(options: TabMouseBaseOptions): Promise<void>;
     click(options: TabMouseBaseOptions & {
         delay?: number;
@@ -19,46 +65,22 @@ type TabNavigationOptions = Protocol.Page.NavigateRequest & {
     waitUntil?: 'documentloaded' | 'load';
 };
 
-interface TabSessionZoneMaker {
-    sessionZone<T>(tabId: string, callback: (session: CDP.Client) => Promise<T>): Promise<T>;
-}
-declare class TabHelper implements TabHandlerInterface, TabSessionZoneMaker {
-    private chromeSessionPort;
-    private onClose?;
-    constructor(chromeSessionPort: number, onClose?: ((tabId: string) => Promise<void>) | undefined);
-    provideMouseHandler(tabId: string): TabMouseHandler;
-    newTab(options: {
-        url: string;
-    }): Promise<Tab>;
-    sessionZone<T>(tabId: string, callback: (session: CDP.Client) => Promise<T>): Promise<T>;
-    navigate(tabId: string, options: TabNavigationOptions): Promise<void>;
-    addScriptToRunOnNewDocument(script: string | TabEvaluateFunction, tabId: string): Promise<void>;
-    evaluateScriptOnTab(script: string | TabEvaluateFunction, tabId: string, _shouldAwait?: boolean): Promise<any>;
-    waitUntilNetworkIdle(tabId: string, options: WaitUntilNetworkIdleOptions): Promise<void>;
-    waitForSelectorAppear(tabId: string, selector: string, options?: WaitForSelectorOptions): Promise<void>;
-    close(tabId: string): Promise<void>;
-}
-
-type TabEvaluateFunction = (...args: any[]) => any;
-declare class Tab {
-    private _tabId;
-    private helper;
-    constructor(_tabId: string, helper: TabHelper);
+type TabEvaluateFunction<T = any, P = any> = (...args: T[]) => P;
+interface Tab extends Evaluable {
+    tabId: string;
+    mouseHandler: TabMouseHandler;
     navigate(options: TabNavigationOptions): Promise<void>;
-    waitForSelectorAppear(selector: string, options?: WaitForSelectorOptions): Promise<void>;
-    evaluate(script: TabEvaluateFunction | string): Promise<any>;
-    get tabId(): string;
-    private _mouseHandler?;
-    get mouseHandler(): TabMouseHandler;
-    close(): Promise<void>;
+    waitForSelectorAppear(selector: string, options?: PollWaitForOptions): Promise<void>;
+    waitUntilReturnTrue(script: string | TabEvaluateFunction, options?: PollWaitForOptions): Promise<void>;
     addScriptToRunOnNewDocument(script: string | TabEvaluateFunction): Promise<void>;
-    waitUntilNetworkIdle(options?: WaitUntilNetworkIdleOptions): Promise<void>;
+    waitUntilNetworkIdle(options: WaitUntilNetworkIdleOptions): Promise<void>;
+    close(): Promise<void>;
 }
 interface WaitUntilNetworkIdleOptions {
     idleInterval: number;
     idleNumber?: number;
 }
-interface WaitForSelectorOptions {
+interface PollWaitForOptions {
     pollInterval?: number;
     waitTimeOut?: number;
 }
@@ -81,7 +103,6 @@ declare class Browser implements TabHandlerInterface {
     private window;
     private tabHandler;
     private init;
-    private defaultTabConsumed;
     newTab(options?: {
         url: string;
     }): Promise<Tab>;
@@ -90,4 +111,4 @@ declare class Browser implements TabHandlerInterface {
     getAllOpenTabs(): Tab[];
 }
 
-export { Tab, type TabEvaluateFunction, type TabNavigationOptions, Browser as default };
+export { type Tab, type TabEvaluateFunction, type TabNavigationOptions, Browser as default };
